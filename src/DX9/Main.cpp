408ebf5fe007f0de69bbb707512d0cc0d8a65f3f
s_aoyama <bleutruth@gmail.com>
2012-06-19 03:25:46 +0900

    ひとまずWinMain関数を追加。

---
+++ src/DX9/Main.cpp
@@ -0,0 +1,13 @@
+//!
+//!	@file	Main.cpp
+//!	@brief	WinMain
+//!
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
+
+/*_________EOF_______________________________________________________________*/
