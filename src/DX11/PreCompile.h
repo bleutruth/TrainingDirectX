4ed2648bda414eed7249819b578804e3a385ed6f
s_aoyama <bleutruth@gmail.com>
2012-06-19 09:55:28 +0900

    プリコンパイルを行うようにした。

---
+++ src/DX11/PreCompile.h
@@ -0,0 +1,10 @@
+//!
+//!	@file	PreCompile.h
+//!	@brief	プリコンパイルヘッダ
+//!
+
+#ifndef DX11_PRECOMPILE_H_20120619
+#define DX11_PRECOMPILE_H_20120619
+
+#endif	//	DX11_PRECOMPILE_H_20120619
+/*__________EOF______________________________________________________________*/
---
+++ src/DX9/PreCompile.h
@@ -0,0 +1,10 @@
+//!
+//!	@file	PreCompile.h
+//!	@brief	プリコンパイルヘッダ
+//!
+
+#ifndef DX9_PRECOMPILE_H_20120619
+#define DX9_PRECOMPILE_H_20120619
+
+#endif	//	DX9_PRECOMPILE_H_20120619
+/*__________EOF______________________________________________________________*/
